0f3bbba9d3d1546fed0320ec49cc10d5721838dd
mfang <mfang@alpine-usa.com>
2014-04-05 09:03:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApnSiswanto::Application.config.secret_key_base = '8e99d9e1b150ed93da9d16e2955593aa9f17b99daecd2ef404dc7912ffa00204a5ec4b1d3d58f4ba03e4a3a1feaf641736146964de8932f9d8cc4ea0eff1632c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApnSiswanto::Application.config.session_store :cookie_store, key: '_apn-siswanto_session'
